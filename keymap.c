8443522f270e300c6630249180910ee6b2bfd021
Wouter de Jong <wouter@wouterj.nl>
2024-12-28 13:34:02 +0100

    Remove alternative, unused, base layers

--- keymap.c
+++ keymap.c
@@ -17,9 +17,9 @@
 #include QMK_KEYBOARD_H
 #include "vendor/kyria-keymap/features/casemodes.h"
 
-enum planck_layers { _QWERTY, _QWERTY_WIDE, _DVORAK, _LOWER, _RAISE, _ADJUST };
+enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST };
 
-enum planck_keycodes { QWERTY = SAFE_RANGE, DVORAK, BACKLIT, XCASE };
+enum planck_keycodes { BACKLIT = SAFE_RANGE, XCASE };
 
 enum { TD_PRN, TD_BRC, TD_CBR, TD_QUOT, TD_GRAV };
 
@@ -35,19 +35,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     OSM(MOD_LSFT), KC_Z,  KC_X,          KC_C,          KC_V,  KC_B,          KC_N,          KC_M,  KC_COMM,         KC_DOT,          KC_SLSH, KC_RSFT,
     OSM(MOD_LCTL), XCASE, OSM(MOD_LALT), OSM(MOD_LGUI), LOWER, SFT_T(KC_ENT), SFT_T(KC_SPC), RAISE, RGUI_T(KC_LEFT), RALT_T(KC_DOWN), KC_UP,   RCTL_T(KC_RGHT)
 ),
-
-[_QWERTY_WIDE] = LAYOUT_planck_grid(
-    KC_Q,  KC_W,          KC_E,          KC_R,  KC_T,   KC_TAB,               KC_BSPC,        KC_Y,  KC_U,            KC_I,            KC_O,    KC_P,
-    KC_A,  KC_S,          KC_D,          KC_F,  KC_G,   KC_ESC,               TD(TD_QUOT),    KC_H,  KC_J,            KC_K,            KC_L,    KC_SCLN,
-    KC_Z,  KC_X,          KC_C,          KC_V,  KC_B,   OSM(MOD_LSFT),        RSFT_T(KC_ENT), KC_N,  KC_M,            KC_COMM,         KC_DOT,  KC_SLSH,
-    OSM(MOD_LCTL), XCASE, OSM(MOD_LALT), OSM(MOD_LGUI), LOWER, SFT_T(KC_ENT), SFT_T(KC_SPC),  RAISE, RGUI_T(KC_LEFT), RALT_T(KC_DOWN), KC_UP,   RCTL_T(KC_RGHT)
-),
-
-[_DVORAK] = LAYOUT_planck_grid(
-    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,  KC_Y,    KC_F,   KC_G,  KC_C,    KC_R,    KC_L,  KC_BSPC,
-    KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,  KC_I,    KC_D,   KC_H,  KC_T,    KC_N,    KC_S,  KC_SLSH,
-    KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,  KC_X,    KC_B,   KC_M,  KC_W,    KC_V,    KC_Z,  KC_RSFT,
-    KC_LCTL, XCASE,   KC_LALT, KC_LGUI, LOWER, KC_RSFT, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
@@ -66,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_ADJUST] = LAYOUT_planck_grid(
     _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
-    _______, EE_CLR,  MU_NEXT, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, DVORAK,  _______, _______,
+    _______, EE_CLR,  MU_NEXT, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
     _______, AU_PREV, AU_NEXT, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
@@ -87,19 +74,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case XCASE:
             if (record->event.pressed) {
                 enable_xcase();
-            }
-            return false;
-            break;
-        case QWERTY:
-            if (record->event.pressed) {
-                print("mode just switched to qwerty and this is a huge string\n");
-                set_single_persistent_default_layer(_QWERTY);
-            }
-            return false;
-            break;
-        case DVORAK:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_DVORAK);
             }
             return false;
             break;
