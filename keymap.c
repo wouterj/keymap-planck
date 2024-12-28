d46882beece0803cece35028d0c436dbee428b1c
Wouter de Jong <wouter@wouterj.nl>
2024-12-28 14:44:21 +0100

    Introduce the gaming layer
    All tab-hold configurations on the keyboard have broken using the
    keyboard for games. Holding the A key no longer dispatches holding the
    left key to the computer, but instead dispatches holding the Alt key.
    
    The gaming layer is a copy of the base layer, with all special hold
    configurations removed. It is enabled as a new default layer, and allows
    you to toggle back to the normal qwerty layer when you're done gaming.
    The keyboard backlights indicate whether or not the gaming layer is
    activated.

--- keymap.c
+++ keymap.c
@@ -17,9 +17,9 @@
 #include QMK_KEYBOARD_H
 #include "vendor/kyria-keymap/features/casemodes.h"
 
-enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST };
+enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST, _GAMING };
 
-enum planck_keycodes { BACKLIT = SAFE_RANGE, XCASE };
+enum planck_keycodes { QWERTY = SAFE_RANGE, GAMING, BACKLIT, XCASE };
 
 enum { TD_PRN, TD_BRC, TD_CBR, TD_QUOT, TD_GRAV };
 
@@ -50,11 +50,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,     _______,       _______,       _______,       _______,       _______, _______, _______,         KC_HOME, KC_PGDN,    KC_PGUP, KC_END
 ),
 
+[_GAMING] = LAYOUT_planck_grid(
+    KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,  KC_T,          KC_Y,          KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,   KC_S,    KC_D,    KC_F,  KC_G,          KC_H,          KC_J,  KC_K,    KC_L,    KC_SCLN, TD_QUOT,
+    KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,  KC_B,          KC_N,          KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LCTL, QWERTY, KC_LALT, KC_LGUI, LOWER, SFT_T(KC_ENT), SFT_T(KC_SPC), RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
+
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
+    _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______,
     _______, EE_CLR,  MU_NEXT, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
     _______, AU_PREV, AU_NEXT, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, GAMING,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 };
@@ -73,6 +80,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case XCASE:
             if (record->event.pressed) {
                 enable_xcase();
+            }
+            return false;
+            break;
+        case QWERTY:
+            if (record->event.pressed) {
+                rgblight_disable();
+                set_single_persistent_default_layer(_QWERTY);
+            }
+            return false;
+            break;
+        case GAMING:
+            if (record->event.pressed) {
+                rgblight_enable();
+                set_single_persistent_default_layer(_GAMING);
             }
             return false;
             break;
